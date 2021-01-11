b2995616086308a6a8081336b7f40fe107ea2141
Michael Brave <michaelbrave@gmail.com>
2021-01-10 22:19:02 -0600

    runs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react_tic-tac-toe
+
+tic tac toe built using this tutorial  
+https://blog.alexdevero.com/how-to-build-simple-tic-tac-toe-game-with-react/
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+///
+// src/index.jsx
+///
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Import Board and Scoreboard views
+import { Board } from './components/board'
+import { Scoreboard } from './components/scoreboard'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import './styles/board.css'
+import './styles/box.css'
+import './styles/buttons.css'
+
+// Create App component
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Route exact path="/" component={Scoreboard}/>
+          <Route path="/board" component={Board}/>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+// Render the App component into DOM
+ReactDOM.render(<App />, document.getElementById('root'))
